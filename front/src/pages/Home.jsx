94dd5e250316d4bbdba7fef20a580547dadbb4b5
Julien Bonneville <jbonneville@systra.com>
2026-02-23 17:05:16 +0100

    rework in Home page

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,10 +2,11 @@ import Paper from '@mui/material/Paper'
 import Button from '@mui/material/Button'
 import logo from '@static/logo.svg'
 import PostAddIcon from '@mui/icons-material/PostAdd'
+import { NavLink } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className="home">
+    <div className="view">
       <Paper elevation="5">
         <div>
           Welcome to
@@ -16,15 +17,18 @@ export default function Home() {
         <p>
           Select a blog post in the list, or create a new one!
         </p>
-        <Button
-          variant="contained"
-          startIcon={<PostAddIcon />}
-          component="a"
-          href="/add-post"
-        >
-          Create a new blog post
-        </Button>
-        <img src={logo} className="react-logo" alt="logo" />
+        <NavLink to="/add-post">
+          <Button
+            variant="contained"
+            startIcon={<PostAddIcon />}
+            component="a"
+          >
+            Create a new blog post
+          </Button>
+        </NavLink>
+        <div>
+          <img src={logo} className="react-logo" alt="logo" />
+        </div>
       </Paper>
       <Paper elevation="5">
         TEST
