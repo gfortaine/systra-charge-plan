d1356ed5610fcac3d7b0045119ba545fb2950b71
Julien Bonneville <jbonneville@systra.com>
2026-03-05 11:48:43 +0100

    Better display for pickers

--- front/src/pages/Pickers.jsx
+++ front/src/pages/Pickers.jsx
@@ -1,25 +1,22 @@
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
-import { Card, CardContent } from '@mui/material'
+import { Paper } from '@mui/material'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { TimePicker } from '@mui/x-date-pickers/TimePicker'
 import { StaticTimePicker } from '@mui/x-date-pickers/StaticTimePicker'
+import './Pickers.scoped.scss'
 
 export default function Pickers() {
   return (
     <div className="view">
       <LocalizationProvider dateAdapter={AdapterMoment}>
-        <Card className="pickers-card">
-          <CardContent>
-            <DatePicker />
-            <TimePicker label="Basic time picker" />
-          </CardContent>
-        </Card>
-        <Card className="pickers-card">
-          <CardContent>
-            <StaticTimePicker />
-          </CardContent>
-        </Card>
+        <Paper className="pickers-paper">
+          <DatePicker />
+          <TimePicker label="Basic time picker" />
+        </Paper>
+        <Paper className="pickers-paper">
+          <StaticTimePicker />
+        </Paper>
       </LocalizationProvider>
     </div>
   )
